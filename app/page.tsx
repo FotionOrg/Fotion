a8d9f3e7d6b257335784a7ae5dedfdcf4830a4fe
junhodo <djh10209@gmail.com>
2025-05-08 00:05:39 +0900

    remove build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,19 @@
 "use client";
-import { ChatContainer } from "@/components/chat-container";
 export default function Home() {
-  return (
-    <>
-      <main className="flex-1 flex flex-col px-2 items-center justify-center">
-        <h1 className="text-5xl font-bold pb-2">
-          Chirpify, Learn English by <span style={{ color: "#0EF397" }}>Thinking</span>
-        </h1>
-        <h2 className="font-medium text-2xl mb-4 pb-4" style={{ color: "#7B7B7B" }}>
-          A new way to learn through exploration and active engagement.
-        </h2>
-
-        <h2 className="font-medium text-2xl mb-4">
-          Try your first lesson for free!
-        </h2>
-        <ChatContainer persona="aru" mode="trial" />
-      </main>
-    </>
-  );
+    return (
+        <>
+            <main className="flex-1 flex flex-col px-2 items-center justify-center">
+                <h1 className="text-5xl font-bold pb-2">
+                    Chirpify, Learn English by <span style={{color: "#0EF397"}}>Thinking</span>
+                </h1>
+                <h2 className="font-medium text-2xl mb-4 pb-4" style={{color: "#7B7B7B"}}>
+                    A new way to learn through exploration and active engagement.
+                </h2>
+                
+                <h2 className="font-medium text-2xl mb-4">
+                    Try your first lesson for free!
+                </h2>
+            </main>
+        </>
+    );
 }
