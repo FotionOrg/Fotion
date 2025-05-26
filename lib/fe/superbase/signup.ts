4cd4c43bc5bb9c2e19390c41c0be9969d1a28a80
junhodo <djh10209@gmail.com>
2025-05-27 04:19:28 +0900

    fix

--- lib/fe/superbase/signup.ts
+++ lib/fe/superbase/signup.ts
@@ -1,16 +1,14 @@
 "use client";
 import { createClient } from "@/lib/fe/superbase/client";
-import { env } from "@/lib/be/utils/env";
 
 export async function signUpWithGoogle() {
   const supabase = await createClient();
-  var protocol = env.isLocal() ? "http" : "https";
 
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${protocol}://${env.nextPublicDomain}/api/auth/callback/google`,
+      redirectTo: `${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/callback/google`,
     },
   });
 
