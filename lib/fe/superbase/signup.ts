2470db6835b73f71c60d145805e5cd39dd9fd4a9
junhodo <djh10209@gmail.com>
2025-05-27 04:39:51 +0900

    fix

--- lib/fe/superbase/signup.ts
+++ lib/fe/superbase/signup.ts
@@ -8,7 +8,7 @@ export async function signUpWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_DOMAIN}/api/auth/callback/google`,
+      redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/api/auth/callback/google`,
     },
   });
 
