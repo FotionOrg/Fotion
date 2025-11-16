94ab3f4841107d1864fefc97b98c547b52350dcd
bm150 <jbm20dev@gmail.com>
2025-11-16 18:05:24 +0900

    Feat : lint 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,19 @@
 import type { NextConfig } from "next";
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from "next-intl/plugin";
+import bundleAnalyzer from "@next/bundle-analyzer";
 
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
 // Bundle Analyzer 설정
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === "true",
 });
 
 const nextConfig: NextConfig = {
   /* config options here */
 
   // Bundle Analyzer를 위해 Webpack 모드 강제 (WEBPACK_BUILD=true일 때)
-  ...(process.env.WEBPACK_BUILD === 'true' && {
+  ...(process.env.WEBPACK_BUILD === "true" && {
     webpack(config) {
       return config;
     },
